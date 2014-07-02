20941cf12c3aae53f63eb66a42ffc52e8503cb24
杨佳佳 <171956531@qq.com>
2014-07-02 17:24:53 +0800

    the last refreshed sever.c
    Comepare this programme with the fomer one ,it adds a new function "sprintf" ，and it gets the goal successfully.Computer B can receive the data of mouse in computer A ！

--- serve.c
+++ serve.c
@@ -7,8 +7,6 @@
 #include <netinet/in.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
-
- 
 #include <linux/input.h>  
 #include <fcntl.h>  
 #include <sys/time.h>   
@@ -29,20 +27,22 @@ int main(int argc,char **argv)
  char buf[6];  
  fd_set readfds;  
  struct timeval tv;  
- fd = open( "/dev/input/mice", O_RDONLY );   
+ fd = open( "/dev/input/mice", O_RDONLY );    
     
  char sdbuf[LENGTH];
  struct sockaddr_in addr_local;
  struct sockaddr_in addr_remote;
  char sendstr[16]={"123456789abcde"};
  
- if(fd<0) {  
-           printf("Failed to open \"/dev/input/mice\".\n");  
-           exit(1);  
-       } 
-else {  
-           printf("open \"/dev/input/mice\" successfuly.\n");  
-     }   
+ if(fd<0) 
+  {  
+      printf("Failed to open \"/dev/input/mice\".\n");  
+      exit(1);  
+  } 
+ else
+  {  
+      printf("open \"/dev/input/mice\" successfuly.\n");  
+    }   
 
  if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)
  {
@@ -74,53 +74,49 @@ else {
  while(1)
  {
   sin_size=sizeof(struct sockaddr);
- if((nsockfd=accept(sockfd,(struct sockaddr *)&addr_remote,&sin_size))==-1)
-  { 
-   printf("ERROR!\n");
-   continue;
-  }
+  if((nsockfd=accept(sockfd,(struct sockaddr *)&addr_remote,&sin_size))==-1)
+   { 
+      printf("ERROR!\n");
+      continue;
+   } 
  else
-  printf("OK:%s!\n",inet_ntoa(addr_remote.sin_addr));
+      printf("OK:%s!\n",inet_ntoa(addr_remote.sin_addr));
  
- if(!fork())
- {
-  printf("Mr Jiajia You can enter string,and press 'exit' to end the connect.\n");
- //while(strcmp(sdbuf,"exit")!=0)
-  //{
- // scanf("%s",sdbuf);
-  //if(num=send(nsockfd,sdbuf,strlen(sdbuf),0)==-1)
-   //{
-  //printf("ERROR:Failed to send string.\n");
-  //close(sockfd);
-  //exit(1);
-   //}
-
- //printf("Mr Yangjiajia:Send %d bytes successfully,please enter gain!\n",num);
-  //}
-  
-  while(1) 
-{  
+  if(!fork())
+  {
+     printf("Mr Jiajia You can enter string,and press 'exit' to end the connect.\n");
+ 
+   while(1) 
+     {  
          tv.tv_sec = 5;  
          tv.tv_usec = 0;  
          FD_ZERO( &readfds );  
          FD_SET( fd, &readfds );  
          retval = select( fd+1, &readfds, NULL, NULL, &tv );  
-         if(retval==0) {  
-         printf( "Time out!\n" );  
-       }  
-          if(FD_ISSET(fd,&readfds)) {   
-             if(read(fd, buf, 6) <= 0) {  
-                continue;  
-            }  
+         if(retval==0) 
+         {  
+             printf( "Time out!\n" );  
+         }  
+         if(FD_ISSET(fd,&readfds)) 
+             {   
+                if(read(fd, buf, 6) <= 0) 
+                {  
+                   continue;  
+                }  
              
-  printf("Button type = %d, X = %d, Y = %d, Z = %d\n", (buf[0] & 0x07), buf[1], buf[2],   buf[3]);  
-          }  
-    }  
+                sprintf(sdbuf,"Button type = %d, X = %d, Y = %d, Z = %d\n", (buf[0] & 0x07), buf[1], buf[2],   buf[3]);  
+  
+                if(num=send(nsockfd,sdbuf,strlen(sdbuf),0)==-1)
+                {
+                     printf("ERROR:Failed to send string.\n");
+                     close(sockfd);
+                     exit(1);
+                }
+             }  
+      }  
      close(fd);  
- }
- close (nsockfd);
- while(waitpid(-1,NULL,WNOHANG)>0);
- }
-}
-
-
+    }
+   close (nsockfd);
+   while(waitpid(-1,NULL,WNOHANG)>0);
+   }
+  }
